eb5f3b556a794b74f55341a6191424ebc116f988
Kevin <lopezkevin175@gmail.com>
2018-06-25 01:10:36 -0400

    added some html title attributes

---
+++ src/javascript/about.js
@@ -0,0 +1,4 @@
+window.addEventListener("load", () => {
+    document.getElementsByClassName("header")[0].classList.add("animate");
+    document.getElementsByClassName("header-desktop")[0].classList.add("animate");                               
+});
